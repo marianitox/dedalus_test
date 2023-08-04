925922799372d331c37bf555c80e35b52ee18dac
MarianoMoron <moron.minoldo@gmail.com>
2023-08-04 19:39:10 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PatientDetailsComponent } from './pages/patient-details/patient-details.component';
+import { PatientListComponent } from './pages/patient-list/patient-list.component';
+import { PAGES } from './shared/constants';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: PAGES.list,
+    pathMatch: 'full'
+  },
+  {
+    path: PAGES.list,
+    component: PatientListComponent
+  },
+  {
+    path: PAGES.detail,
+    component: PatientDetailsComponent
+  },
+  {
+    path: '**',
+    redirectTo: PAGES.list,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/constants.ts
@@ -0,0 +1,4 @@
+export const PAGES = {
+  list: 'list',
+  detail: 'detail'
+};
