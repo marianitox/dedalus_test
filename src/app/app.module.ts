9117120dd49c03061248f78075b3a6301143d4f3
MarianoMoron <moron.minoldo@gmail.com>
2023-08-04 19:31:24 -0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PatientListComponent } from './pages/patient-list/patient-list.component';
+import { PatientDetailsComponent } from './pages/patient-details/patient-details.component';
+import { TableComponent } from './shared/components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PatientListComponent,
+    PatientDetailsComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
